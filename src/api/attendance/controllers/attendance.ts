d1518306387cc2a6f717019f15c5777b3e96a7d7
umairservices <umairservices321@gmail.com>
2025-07-11 14:29:56 +0500

    changess

--- src/api/attendance/controllers/attendance.ts
+++ src/api/attendance/controllers/attendance.ts
@@ -1,54 +1,7 @@
-// ./src/api/attendance/controllers/attendance.ts
+/**
+ * attendance controller
+ */
+
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController(
-  "api::attendance.attendance",
-  ({ strapi }) => ({
-    // GET /attendances
-    async find(ctx: any) {
-      const user = ctx.state.user;
-      if (user) {
-        // start from whatever filters exist (or empty object)
-        const existingFilters: Record<string, any> = ctx.query.filters || {};
-
-        ctx.query.filters = {
-          ...existingFilters,
-          student: {
-            createdBy: {
-              id: { $eq: ctx.state.user.id },
-            },
-          },
-        };
-
-        // ensure we populate student.createdBy so the filter works
-        ctx.query.populate = { student: { populate: ["createdBy"] } };
-      }
-
-      return await super.find(ctx);
-    },
-
-    // GET /attendances/:id
-    async findOne(ctx: any) {
-      const user = ctx.state.user;
-
-      // load the record with its student.createdBy
-      const attendance: any = await strapi.entityService.findOne(
-        "api::attendance.attendance",
-        ctx.params.id,
-        { populate: { student: { populate: ["createdBy"] } } }
-      );
-
-      // now we can safely index into .student.createdBy
-      if (
-        !attendance ||
-        attendance.student.createdBy.id.toString() !== user.id.toString()
-      ) {
-        return ctx.unauthorized(
-          "You can only view attendance for students you created"
-        );
-      }
-
-      return await super.findOne(ctx);
-    },
-  })
-);
+export default factories.createCoreController("api::attendance.attendance");
